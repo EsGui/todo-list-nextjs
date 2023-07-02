4cf51acfe752d54a8e33e47eb37506f29d130ce7
Guilherme Azevedo da Gama <guilhermedagama2021100@gmail.com>
2023-07-02 05:12:07 -0300

    feat: salvando alterações, criei objeto com os games

---
+++ app/objects/games-animes.js
@@ -0,0 +1,37 @@
+const gamesAnimes = [
+    {
+        id: 1,
+        name: "League of legends",
+        img: "http://localhost:3000/league-of-legends/imgs/league-of-legends.jpg",
+        description: "League of Legends é um jogo de estratégia em que duas equipes de cinco poderosos Campeões se enfrentam para destruir a base uma da outra. Escolha entre mais de 140 Campeões para realizar jogadas épicas, assegurar abates e destruir torres conforme você luta até a vitória.",
+        trailer: "QteaiF7Fvrg",
+    },
+    {
+        id: 2,
+        name: "Overwatch 2",
+        img: "http://localhost:3000/overwatch2/imgs/overwatch2.jpg",
+        description: "Overwatch 2 é um jogo de ação em equipe grátis para jogar que se passa em um futuro otimista, no qual cada partida é um campo de batalha 5v5 definitivo.",
+        trailer: "tgYIz_UVRT4"
+    },
+    {
+        id: 3,
+        name: "Valorant",
+        img: "http://localhost:3000/valorant/imgs/valorant.avif",
+        description: "Valorant é um FPS tático 5x5 que tem como objetivo plantar ou desarmar a Spike. Os jogadores têm apenas uma vida por rodada e a partida é vencida pela equipe que ganhar 13 rodadas (de 25) primeiro.",
+        trailer: "a4ccvu52uDY",
+    },
+    {
+        id: 4,
+        name: "Minecraft",
+        img: "http://localhost:3000/minecraft/imgs/minecraft.png",
+        description: "Minecraft é um jogo eletrônico lançado em 2009 que consiste em sobreviver em um mundo formado (majoritariamente) por blocos cúbicos. Steve, o personagem controlado pelo jogador, inicia o jogo em um ambiente repleto de árvores, montanhas, rios.",
+        trailer: "Rla3FUlxJdE",
+    },
+    {
+        id: 5,
+        name: "CS GO",
+        img: "http://localhost:3000/cs-go/imgs/cs-go.jpg",
+        description: "CS:GO é um FPS da Valve disputado entre duas equipes. Elas se revezam entre terroristas e contra terroristas, os terroristas armam a bomba enquanto o outro time protege os sites, locais onde os explosivos devem ser plantados. Alguns mapas funcionam com reféns, que por sua vez devem ser salvos pelos contra terroristas.",
+        trailer: "edYCtaNueQY"
+    },
+]
