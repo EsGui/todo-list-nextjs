c8d9a2c74f9eb047a3024e39d4b1b4d7cbc68ccc
Guilherme Azevedo da Gama <guilhermedagama2021100@gmail.com>
2023-07-04 15:09:12 -0300

    feat: link das redes foi adicionado

--- app/components/home_components/HomeFooter.js
+++ app/components/home_components/HomeFooter.js
@@ -1,16 +1,24 @@
 import { redes } from '@/app/objects/redes'
 import styles from '../../styles/home_styles/HomeFooter.module.css'
+import Link from 'next/link'
 
 export default function HomeFooter() {
+    const styleLink = {
+        textDecoration: "none",
+        color: "white",
+    }
+
     return (
         <div className={ styles.DivFatherHomeFooter }>
             <div className={ styles.DivRedesHomeFooter }>
                 {
-                    redes.map(({ name, image }) => (
-                        <div>
-                            <img src={ image } alt={ name } />
-                            <p>{name}</p>
-                        </div>
+                    redes.map(({ name, image, link }) => (
+                        <Link style={ styleLink } href={ link }>
+                            <div>
+                                <img src={ image } alt={ name } />
+                                <p>{name}</p>
+                            </div>
+                        </Link>
                     ))
                 }
             </div>
--- app/objects/redes.js
+++ app/objects/redes.js
@@ -2,10 +2,11 @@ export const redes = [
     {
         name:"GitHub",
         image: "http://localhost:3000/redes/github.png",
-        link: ""
+        link: "https://github.com/EsGui/todo-list-nextjs"
     },
     {
         name: "Linkedin",
-        image: "http://localhost:3000/redes/linkedin.png"
+        image: "http://localhost:3000/redes/linkedin.png",
+        link: "https://www.linkedin.com/in/guilherme-azevedo-442170221/"
     },
 ];
